2506ed1a03956400281cb6c4f32e2ea8f13e868a
刘润泽 <1414556676@qq.com>
2022-01-21 17:23:23 +0800

    clear history

--- .history/tk_20211209180058.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-
-# reference: https://tkdocs.com/tutorial/
-
-# test tk pop up
-# tkinter._test()
-
-tkinter.Tcl().eval('info patchlevel')
--- .history/tk_20211209180238.py
+++
@@ -1,6 +0,0 @@
-import tkinter
-
-# reference: https://tkdocs.com/tutorial/
-
-# test tk pop up
-tkinter._test()
