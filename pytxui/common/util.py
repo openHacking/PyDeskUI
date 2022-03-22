87c8b9d1e62d98b6422e8981cdcfef4044550ac4
openHacking <openHacking@126.com>
2022-03-22 22:26:13 +0800

    fix(change pointinghand cursor on mac)

--- pytxui/common/util.py
+++ pytxui/common/util.py
@@ -1,3 +1,5 @@
+import platform
+
 # https://stackoverflow.com/a/29643643
 def hex_to_rgb(h):
     return tuple(int(h[1:][i:i+2], 16) for i in (0, 2, 4))
@@ -17,3 +19,13 @@ def interpolate(color_a, color_b, t):
         list.append(i)
     return tuple(list)
     # return tuple( int(a + (b - a) * t) for a, b in zip(color_a, color_b))
+
+# reference: https://docs.python.org/3/library/platform.html#module-platform
+def get_os_type():
+    os_type = platform.system()
+    if os_type == 'Windows':
+        return 'windows'
+    elif os_type == 'Darwin':
+        return 'mac'
+    elif os_type == 'Linux':
+        return 'linux'
